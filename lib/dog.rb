544fddcc6e5ea5e5553b0c937f3cf8817204066a
Uma Manandhar <um_2003_8@hotmail.com>
2019-11-24 20:45:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,30 @@
-class Dog
-  def name= (dog_name)
-    @dog_name = name
-  end
-
-  def name
-    @name
-  end
-  dog_name.instance_variable_det(:@name)
-
-end
+# class Dog
+#   def name= (dog_name)
+#     @dog_name = name
+#   end
+#
+#   def name
+#     @name
+#   end
+#
+# end
 
 
 # dog_name = Dog.new
 # dog_name.name = "dog_name"
 # puts dog_name.name
+class Dog
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+end
