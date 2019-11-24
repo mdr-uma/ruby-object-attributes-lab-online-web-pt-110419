35853503dcbf0e4629adc4b56768a953fd165a8e
Uma Manandhar <um_2003_8@hotmail.com>
2019-11-24 20:51:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,32 +1,15 @@
-# class Dog
-#   def name= (dog_name)
-#     @dog_name = name
-#   end
-#
-#   def name
-#     @name
-#   end
-#
-# end
-
-
-
 class Dog
-
-  def initialize(name)
-    @name = name
+  def name= (dog_name)
+    @name = dog_name
   end
 
   def name
     @name
   end
 
-  def name=(new_name)
-    @name = new_name
-  end
-
 end
 
-new_name = Dog.new
-new_name.name = "new_name"
-puts new_name.name
+
+# dog_name = Dog.new
+# dog_name.name = "dog_name"
+# puts dog_name.name
